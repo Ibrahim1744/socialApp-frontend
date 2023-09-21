dc3db9081305262dad32f6d3e26aad60ada3abf7
Ibrahim1744 <himanaser86@gmail.com>
2023-09-21 05:59:33 +0300

    ready to be deployed

---
+++ src/pages/Home/Home.js
@@ -0,0 +1,18 @@
+import React from 'react';
+import './home.css'
+const Home = () => {
+  return (
+    <div className='parent'>
+      
+      <div className='left'>
+      <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Dignissimos sit nihil saepe eum at earum illum quas voluptasm ipsum dolor sit, amet consectetur adipisicing elit. Dignissimos sit nihil saepe eum at earum illum quas voluptasm ipsum dolor sit, amet consectetur adipisicing elit. Dignissimos sit nihil saepe eum at earum illum quas voluptasm ipsum dolor sit, amet consectetur adipisicing elit. Dignissimos sit nihil saepe eum at earum illum quas voluptasm ipsum dolor sit, amet consectetur adipisicing elit. Dignissimos sit nihil saepe eum at earum illum quas voluptas aperiam minus! Nesciunt alias quod asperiores tempore beatae temporibus minima pariatur a.</p>
+      </div>
+      <div className='right'>
+          <p>Eye news vision</p>
+          <p>Eye news message</p>
+      </div>
+    </div>
+  );
+}
+
+export default Home;
