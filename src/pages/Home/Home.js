89d22f896c5ddc837888a325315741386d09a999
Ibrahim1744 <himanaser86@gmail.com>
2023-09-21 06:12:12 +0300

    home

--- src/pages/Home/Home.js
+++ src/pages/Home/Home.js
@@ -1,18 +1,38 @@
-import React from 'react';
-import './home.css'
+import PostList from "../../components/posts/PostList";
+import "./home.css";
+import Sidebar from "../../components/sidebar/Sidebar";
+import { Link } from "react-router-dom";
+import { useDispatch, useSelector } from "react-redux";
+import { useEffect } from "react";
+import { fetchPosts } from "../../redux/apiCalls/postApiCall";
+
 const Home = () => {
+  const dispatch = useDispatch();
+  const { posts } = useSelector(state => state.post);
+
+  useEffect(() => {
+    dispatch(fetchPosts(1));
+  }, []);
+
   return (
-    <div className='parent'>
-      
-      <div className='left'>
-      <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Dignissimos sit nihil saepe eum at earum illum quas voluptasm ipsum dolor sit, amet consectetur adipisicing elit. Dignissimos sit nihil saepe eum at earum illum quas voluptasm ipsum dolor sit, amet consectetur adipisicing elit. Dignissimos sit nihil saepe eum at earum illum quas voluptasm ipsum dolor sit, amet consectetur adipisicing elit. Dignissimos sit nihil saepe eum at earum illum quas voluptasm ipsum dolor sit, amet consectetur adipisicing elit. Dignissimos sit nihil saepe eum at earum illum quas voluptas aperiam minus! Nesciunt alias quod asperiores tempore beatae temporibus minima pariatur a.</p>
+    <section className="home">
+      <div className="home-hero-header">
+        <div className="home-hero-header-layout">
+          <h1 className="home-title">Welcome to Blog</h1>
+        </div>
       </div>
-      <div className='right'>
-          <p>Eye news vision</p>
-          <p>Eye news message</p>
+      <div className="home-latest-post">Latest Posts</div>
+      <div className="home-container">
+        <PostList posts={posts} />
+        <Sidebar />
       </div>
-    </div>
+      <div className="home-see-posts-link">
+        <Link to="/posts" className="home-link">
+          See All Posts
+        </Link>
+      </div>
+    </section>
   );
-}
+};
 
 export default Home;
